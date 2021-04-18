c40ead50eeb58b26b480878129063b9bb4dcee80
ISMAYIL MAMMADOV <ismayilyashar@gmail.com>
2021-04-17 22:29:55 -0400

    some codes have been modified

--- src/package_001/Class_001.java
+++ src/package_001/Class_001.java
@@ -3,7 +3,17 @@ package package_001;
 public class Class_001 {
 	
 	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int numOfStudents = 71;
+		double num = 34.5;
+		// here are my changes
 		
+
+		for(int i = 0; i < args.length; i++) {
+			
+ }
+
+		//Today we have seen new commands on Git and GitHub
 	}
 
 }
